ee3dd30e0fc4bcc07b793d4ef1a8f5746a7580f4
Tarak Sharma <tarak.scope@gmail.com>
2017-12-25 21:04:59 +0530

    Implemented Library Sorting
    Implemented library sorting based on columns for songs view.

--- src/renderer/components/library/Songs.tsx
+++ src/renderer/components/library/Songs.tsx
@@ -14,6 +14,8 @@ export interface SongsProps {
 
 interface SongsState {
 	tracks: List<TrackModel>;
+	sortBy: string;
+	sortDirection: 'ASC' | 'DESC';
 }
 
 export default class Songs extends React.Component<SongsProps, SongsState> {
@@ -21,12 +23,51 @@ export default class Songs extends React.Component<SongsProps, SongsState> {
 		super(props);
 
 		this.state = {
-			tracks: props.tracks
+			tracks: this.sortList(props.tracks, 'title', 'ASC'),
+			sortBy: 'title',
+			sortDirection: 'ASC'
 		};
 	}
 
 	rowGetter = ({ index }: {index: number}) => {
 		return this.state.tracks.get(index);
+	}
+
+	handleTableSort = ({sortBy, sortDirection}: {sortBy: string, sortDirection: 'ASC'|'DESC'}) => {
+		// Sort List based on parameters provided by table
+		this.setState({
+			tracks: this.sortList(this.state.tracks, sortBy, sortDirection),
+			sortBy: sortBy,
+			sortDirection: sortDirection
+		});
+	}
+
+	sortList = (tracks: List<TrackModel>, sortBy: string, sortDirection: 'ASC'|'DESC') => {
+		if (!sortBy.localeCompare('title')) {
+			return tracks
+			.sortBy(item => item.common['title'])
+			.update(list => (sortDirection === 'DESC' ? list.reverse() : list));
+		} else {
+			/*
+				Sorting the list based on compound attributes such as Artists, Lists, etc.
+				Divides the lists into chunks based on sortBy, sorts the list according to title
+				then merges the list.
+			*/
+			let list: List<TrackModel> = List();
+
+			//Group the list into individual lists on sortBy attribute
+			tracks.groupBy((value) => value.common[sortBy])
+			.sort((a: List<TrackModel>, b: List<TrackModel>) => {
+				//Sort the groups based on the sortBy attribute
+				return a.get(0)!.common[sortBy].localeCompare(b.get(0)!.common[sortBy]);
+			})
+			.forEach((value, key) => {
+				// Merge the individual lists into one
+				list = list.concat(value.toList().sortBy(item => item.common['title']));
+			});
+
+			return list.update(list => (sortDirection === 'DESC' ? list.reverse() : list));
+		}
 	}
 
 	refreshQueue = (info: RowMouseEventHandlerParams) => {
@@ -35,7 +76,7 @@ export default class Songs extends React.Component<SongsProps, SongsState> {
 	}
 
 	componentWillReceiveProps(nextProps: SongsProps) {
-		this.setState({ tracks: nextProps.tracks });
+		this.setState({tracks: this.sortList(nextProps.tracks, this.state.sortBy, this.state.sortDirection)});
 	}
 
 	render() {
@@ -50,6 +91,9 @@ export default class Songs extends React.Component<SongsProps, SongsState> {
 						rowHeight={40}
 						width={width}
 						onRowDoubleClick={this.refreshQueue}
+						sort={this.handleTableSort}
+						sortBy={this.state.sortBy}
+						sortDirection={this.state.sortDirection}
 					>
 						<Column
 							label='Name'
@@ -71,6 +115,7 @@ export default class Songs extends React.Component<SongsProps, SongsState> {
 							// tslint:disable-next-line:max-line-length
 							cellDataGetter={({ rowData }: {rowData: TrackModel}) => TimeUtils.parseToMinutes(rowData.format.duration !== undefined ? rowData.format.duration : 0)}
 							dataKey='duration'
+							disableSort={true}
 							width={100} />
 					</Table>
 				)}
