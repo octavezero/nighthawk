a3d5027340ec727298ac151acb79b4a6552e3068
Tarak Sharma <tarak.scope@gmail.com>
2018-01-20 02:39:30 +0530

    Added Ghost Button Style
    Added the ghost button style.

--- src/renderer/components/common/Button.tsx
+++ src/renderer/components/common/Button.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as classNames from 'classnames';
 
 export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
-	type: 'default' | 'primary' | 'link';
+	type: 'default' | 'primary' | 'link' | 'ghost';
 	icon?: boolean;
 	tooltip?: string;
 	tooltipPosition?: string;
