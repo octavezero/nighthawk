edb87add34445e05737a56b38b4f09f96adc4ee2
Tarak Sharma <tarak.scope@gmail.com>
2018-01-27 00:23:54 +0530

    Popover Blurring Fix [skip ci]
    Inclues a hack to position the queue popover without causing wierd rendering errors. Will be fixed by the next electron upstream release with chromium 61. Also, Positioning is not perfect with hack.

--- src/renderer/components/common/Popover.tsx
+++ src/renderer/components/common/Popover.tsx
@@ -17,7 +17,7 @@ export default class Popover extends React.Component<PopoverProps, PopoverState>
 		super(props);
 
 		this.state = {
-			isActive: false
+			isActive: false,
 		};
 	}
 
@@ -28,7 +28,32 @@ export default class Popover extends React.Component<PopoverProps, PopoverState>
 	render() {
 		const {
 			position
-		  } = this.props;
+		} = this.props;
+
+		let w = window.innerWidth;
+		let h = window.innerHeight;
+
+		//TODO: This is a hack to work around a chromium problem. Will be fixed in the next electron build.
+		let transform: string = 'translate(' + -Math.round(h / 2) + 'px, ' + -Math.round(w / 2) + 'px) scale(0)';
+
+		if (this.state.isActive) {
+			switch (this.props.position) {
+				case 'left':
+					transform = 'translate(-100%, ' + -Math.round(w / 2) + 'px) scale(1)';
+					break;
+				case 'right':
+					transform = 'transform: translate(0, ' + -Math.round(w / 2) + 'px) scale(1):';
+					break;
+				case 'bottom':
+					transform = 'translate(' + -Math.round(h / 2) + 'px, 0) scale(1)';
+					break;
+				case 'top':
+					transform = 'transform: translate(' + -Math.round(h / 2) + 'px, -100%) scale(1)';
+					break;
+				default:
+					break;
+			}
+		}
 
 		let buildClassNames: string = classNames(
 			'popover',
@@ -40,7 +65,7 @@ export default class Popover extends React.Component<PopoverProps, PopoverState>
 			<div className={buildClassNames}>
 				{React.cloneElement(this.props.button as React.ReactElement<any>, {onClick: this.togglePopover})}
 				<div className='popover-overlay' onClick={this.togglePopover}></div>
-				<div className='popover-container'>
+				<div className='popover-container' style={{ transform: transform }}>
 					<div className='popover-body'>
 						{this.props.children}
 					</div>
